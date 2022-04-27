e445f9d54d2ccbaa5c752ee220ec5bfb99668fad
Hamza <Hamza5955>
2022-04-27 03:59:33 +0200

    funge

--- js/main.js
+++ js/main.js
@@ -3,3 +3,9 @@ let menu = document.querySelector(".menu-icon");
 menu.onclick = () => {
   menu.classList.toggle("move");
 };
+
+let header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("header-active", window.scrollY > 0);
+});
